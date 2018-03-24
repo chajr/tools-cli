6083e05fd8eed9915c2ab3b55e1171db812d359f
chajr <chajr@bluetree.pl>
2018-03-24 12:22:24 +0100

    Read and load all tools commands

--- src/Console/Commands.php
+++ src/Console/Commands.php
@@ -3,21 +3,66 @@
 namespace ToolsCli\Console;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use BlueContainer\Container;
+use BlueRegister\Register;
 
 class Commands extends Container
 {
-    protected $data;
+    /**
+     * @var Register
+     */
+    protected $register;
+
+    /**
+     * @var Log
+     */
+    protected $log;
+
+    /**
+     * @var Event
+     */
+    protected $event;
 
     public function __construct()
     {
-        
-        //load tools list
-        
-        
-        parent::__construct();
+        //read configuration
+//        $this->log = new Log([]);
+//        $this->event = new Event([]);
+        $this->register = new Register([]);
+
+        parent::__construct(['data' => $this->readAllCommandTools()]);
+
+        //set default command
+//        $this->set('default_name', 'helper');
+    }
+
+    /**
+     * @return array
+     */
+    protected function readAllCommandTools() : array
+    {
+        $list = [];
+        $fileList = glob('src/Tools/*/*.php');
+
+        foreach ($fileList as $commandFile) {
+            $namespace = str_replace(
+                ['/', 'src', '.php'],
+                ['\\', 'ToolsCli', ''],
+                $commandFile
+            );
+
+            $list[$namespace] = $this->registerCommandTool($namespace);
+        }
+
+        return $list;
+    }
+
+    /**
+     * @param string $namespace
+     * @return Command
+     */
+    protected function registerCommandTool(string $namespace) : Command
+    {
+        return $this->register->factory($namespace);
     }
 }
