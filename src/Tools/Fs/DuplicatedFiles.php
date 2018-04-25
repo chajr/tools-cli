3db67ea70b0c197897fe4a6bac069f0170dcc629
chajr <chajr@bluetree.pl>
2018-04-25 17:43:03 +0200

    Todo move

--- src/Tools/Fs/DuplicatedFiles.php
+++ src/Tools/Fs/DuplicatedFiles.php
@@ -2,9 +2,13 @@
 
 namespace ToolsCli\Tools\Fs;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\{
+    Input\InputInterface,
+    Input\InputArgument,
+    Output\OutputInterface,
+};
+use ToolsCli\Console\Command;
+use ToolsCli\Console\Display\Style;
 
 class DuplicatedFiles extends Command
 {
@@ -13,6 +17,26 @@ class DuplicatedFiles extends Command
         $this->setName('fs:duplicated')
             ->setDescription('Search files duplication and make some action on it.')
             ->setHelp('');
+
+        $this->addArgument(
+            'source',
+            InputArgument::REQUIRED,
+            'source files to convert'
+        );
+
+        $this->addOption(
+            'interactive',
+            'i',
+            null,
+            'ask for deletion of duplicated files'
+        );
+        
+        //inverse mode (default select to delete, in inverse select to keep)
+        
+        //symfony interract checkbox like yoman
+
+        //skipdir??
+        //generate html file with buttons to delete duplicated files
     }
 
     /**
@@ -23,6 +47,14 @@ class DuplicatedFiles extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        //generate html file with possibility to delete file
         
+        //fduper -r (-S)
+        
+        //shell_exec ...
+        
+        //select 1    1,2,3    1-6   a
+        
+        //count files all + deleted
     }
 }
