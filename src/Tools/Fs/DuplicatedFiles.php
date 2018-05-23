64a8b49faef1cee57e40d80a945042d3796d3afb
chajr <chajr@bluetree.pl>
2018-05-23 22:47:24 +0200

    Added basic search for duplicated files

--- src/Tools/Fs/DuplicatedFiles.php
+++ src/Tools/Fs/DuplicatedFiles.php
@@ -8,7 +8,8 @@ use Symfony\Component\Console\{
     Output\OutputInterface,
 };
 use ToolsCli\Console\Command;
-use ToolsCli\Console\Display\Style;
+use BlueFilesystem\Fs;
+use BlueData\Calculation\Math;
 
 class DuplicatedFiles extends Command
 {
@@ -21,19 +22,25 @@ class DuplicatedFiles extends Command
         $this->addArgument(
             'source',
             InputArgument::REQUIRED,
-            'source files to convert'
+            'source files to check'
         );
 
         $this->addOption(
             'interactive',
             'i',
             null,
-            'ask for deletion of duplicated files'
+            'show multi-checkbox with duplicated files, selected will be deleted'
         );
+
+        //skip empty files
+        //show hash
+        //format filesize
+        //check by similar names -ns -ns 50 - percent (int similar_text ( string $first , string $second [, float &$percent ] ))
         
+//
         //inverse mode (default select to delete, in inverse select to keep)
+        //delete and create symbolic link
         
-        //symfony interract checkbox like yoman
 
         //skipdir??
         //generate html file with buttons to delete duplicated files
@@ -47,14 +54,57 @@ class DuplicatedFiles extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $hashes = [];
+        //build list of all files and their paths (bluefs)
+        //foreach on list and calculate hash
+        
+        $list = Fs::readDirectory($input->getArgument('source'), true);
+        $fileList = Fs::returnPaths($list)['file'];
+
+        //echo reading files
+        foreach ($fileList as $file) {
+            $hash = hash_file('sha3-256', $file);
+
+            $hashes[$hash][] = $file;
+        }
+
+        //echo checking files
+        foreach ($hashes as $hash) {
+            if (count($hash) > 1) {
+                $size = filesize(reset($hash));
+                $output->writeln("Duplications ($size):");
+
+                foreach ($hash as $file) {
+                    $output->writeln($file);
+                }
+
+                $output->writeln('');
+            }
+        }
+        
+        
+        
         //generate html file with possibility to delete file
         
-        //fduper -r (-S)
+        //fduper -r 
         
-        //shell_exec ...
         
         //select 1    1,2,3    1-6   a
         
-        //count files all + deleted
+        //count files all, show size + deleted
+        //progress bar (in case of full php without fdupes)
+        //hash_file
+        //hash_equals
+        //set file in array with size, if file with the same size is detected, then calculate hash of that files and check hash
+        //set file path & size in array, size as index, if index exists calculate hashes and add files into array
+        //in seccond iteration check hashes and skip single files
+        
+        
+        //sha2; sha-3
+        //sha3-256, sha384, sha512, sha3-384
+        
+        
+        
+        //count all files after build tree (or find -type f) and progress bar of checking files
     }
 }
