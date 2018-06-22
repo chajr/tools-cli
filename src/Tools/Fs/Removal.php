cffeadca2723543e6bbf68d68262da808a6f791c
chajr <chajr@bluetree.pl>
2018-06-22 19:08:21 +0200

    Basic class for automatic remove

--- src/Tools/Fs/Removal.php
+++ src/Tools/Fs/Removal.php
@@ -5,3 +5,32 @@
  * while fgets(STDIN)
  * -r -- kasuje zawartosc katalogow
  */
+
+namespace ToolsCli\Tools\Fs;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Removal extends Command
+{
+
+
+    protected function configure() : void
+    {
+        $this->setName('fs:removal')
+            ->setDescription('Info.')
+            ->setHelp('');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     * @throws \InvalidArgumentException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
