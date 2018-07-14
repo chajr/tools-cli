bdfa7b56d903c95f58e46757180bd5054b6b573f
chajr <chajr@bluetree.pl>
2018-07-14 08:25:33 +0200

    Added logic to count chars to fix occurence

--- src/Tools/Utils/VideoHexFixerTool.php
+++ src/Tools/Utils/VideoHexFixerTool.php
@@ -60,13 +60,51 @@ class VideoHexFixerTool extends Command
         //check with loop number (binary) and decide to replace or leave hex
         //save file as temp
         //check that file is playable
-
+        echo "read file\n";
         $source = $input->getArgument('source');
         $fileErr = fopen($source, 'rb');
-        $content = fread($fileErr, filesize($source));
-        $content = bin2hex(fread($fileErr, filesize($source)));
-        var_dump(strpos($content, '00'));
+//        $content = fread($fileErr, filesize($source));
+//        var_dump(filesize($source));
+//        $content = bin2hex(fread($fileErr, filesize($source)));
+//        var_dump(strlen($content));
+//        var_dump(bin2hex(fread($fileErr, 100)));
+//        var_dump(substr_count($content, '0d'));
+//        var_dump(substr_count($content, '0a'));
+//        var_dump(strpos($content, '0d', 19207));
         
         
+//        $counter = 0;
+        $nlCharsCount = 0;
+        $crCharsCount = 0;
+        $crBin = hex2bin(0x0d);
+        $nlBin = hex2bin(0x0a);
+
+        $nlPos = [];
+        $crPos = [];
+        
+        $data = '';
+        $size = filesize($source);
+        echo "process file\n";
+        for ($i = 1; $i <= $size; $i++) {
+            $charOk = fgetc($fileErr);
+
+            if ($charOk === $crBin) {
+                $crCharsCount++;
+                $crPos[] = $i -1;
+            }
+            if ($charOk === $nlBin) {
+                $nlCharsCount++;
+                $nlPos[] = $i -1;
+            }
+
+            $data .= $charOk;
+        }
+
+        echo "out\n\n";
+        var_dump($size);
+        var_dump($nlCharsCount);
+        var_dump($crCharsCount);
+        var_dump($i);
+        var_dump(strlen($data));
     }
 }
