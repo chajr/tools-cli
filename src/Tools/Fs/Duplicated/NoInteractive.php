be250fbd27fa0597380cafb6f0211ed35c4ad106
chajr <chajr@bluetree.pl>
2018-07-21 10:09:52 +0200

    ImplementNoInteractive strategy

--- src/Tools/Fs/Duplicated/NoInteractive.php
+++ src/Tools/Fs/Duplicated/NoInteractive.php
@@ -2,36 +2,63 @@
 
 namespace ToolsCli\Tools\Fs\Duplicated;
 
-class NoInteractive
+use ToolsCli\Tools\Fs\DuplicatedFilesTool;
+use BlueData\Data\Formats;
+
+class NoInteractive implements Strategy
 {
-    public function __construct()
+    /**
+     * @var \BlueConsole\Style
+     */
+    protected $blueStyle;
+
+    protected $duplicatedFiles = 0;
+    protected $duplicatedFilesSize = 0;
+
+    /**
+     * @param DuplicatedFilesTool $dft
+     */
+    public function __construct(DuplicatedFilesTool $dft)
     {
-        
+        $this->blueStyle = $dft->getBlueStyle();
     }
 
     /**
      * @param array $hashes
      * @return $this
      */
-    protected function checkByHash(array $hashes) : self
+    public function checkByHash(array $hashes) : Strategy
     {
         foreach ($hashes as $hash) {
             if (\count($hash) > 1) {
                 $this->blueStyle->writeln('Duplications:');
 
-                    foreach ($hash as $file) {
-                        $this->duplicatedFiles++;
-                        $size = filesize($file);
-                        $this->duplicatedFilesSize += $size;
+                foreach ($hash as $file) {
+                    $this->duplicatedFiles++;
+                    $size = filesize($file);
+                    $this->duplicatedFilesSize += $size;
 
-                        $formattedSize = Formats::dataSize($size);
-                        $this->blueStyle->writeln("$file ($formattedSize)");
-                    }
+                    $formattedSize = Formats::dataSize($size);
+                    $this->blueStyle->writeln("$file ($formattedSize)");
+                }
 
                 $this->blueStyle->newLine();
             }
         }
 
         return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function returnCounters() : array
+    {
+        return [
+            $this->duplicatedFiles,
+            $this->duplicatedFilesSize,
+            0,
+            0,
+        ];
     }
 }
