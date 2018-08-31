dcd7aa6bf1e52256bcc9cd1a417b29c188caadff
chajr <chajr@bluetree.pl>
2018-08-31 16:08:21 +0200

    Empty tool file

---
+++ src/Tools/Git/Deploy.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+check if current branch is develop
+git push origin develop
+checkout to master
+git merge develop
+git push origin master
+git tag $TAG
+git push --tags
+git checkout develop
+*/
