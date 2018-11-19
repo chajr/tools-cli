cf1f716cb3658a9677731673a8f5db756ee6644d
chajr <chajr@bluetree.pl>
2018-11-19 17:57:59 +0100

    Todo replacement

--- src/Tools/Git/Deploy.php
+++ src/Tools/Git/Deploy.php
@@ -1,12 +1,5 @@
 <?php
 
 /**
-check if current branch is develop
-git push origin develop
-checkout to master
-git merge develop
-git push origin master
-git tag $TAG
-git push --tags
-git checkout develop
+deployment to direct server
 */
