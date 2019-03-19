ee5e2418c8e018357c61c1ddf02fc14fc6cf2ecf
Michał Adamiak <michal.adamiak@lizardmedia.pl>
2019-03-19 13:27:20 +0100

    Fixed dependencies

--- src/Tools/System/ZshHistoryMergTool.php
+++ src/Tools/System/ZshHistoryMergTool.php
@@ -2,7 +2,11 @@
 
 namespace ToolsCli\Tools\System;
 
-class ZshHistoryMergTool
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ZshHistoryMergTool extends Command
 {
     protected function configure() : void
     {
