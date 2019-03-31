4d081d0e710c85a795f822458315e75b9eec58e6
Michał Adamiak <michal.adamiak@lizardmedia.pl>
2019-03-31 11:58:58 +0200

    Refactored building history

--- src/Tools/System/ZshHistoryMergTool.php
+++ src/Tools/System/ZshHistoryMergTool.php
@@ -76,75 +76,26 @@ class ZshHistoryMergTool extends Command
         try {
             $this->formatter = $this->register->factory(FormatterHelper::class);
             $this->blueStyle = $this->register->factory(Style::class, [$input, $output, $this->formatter]);
-//            $this->progressBar = $this->register->factory(ProgressBar::class, [$output]);
         } catch (RegisterException $exception) {
             throw new \Exception('RegisterException: ' . $exception->getMessage());
         }
+
+        $this->blueStyle->infoMessage('Starting merge proces.');
 
         $zshFiles = $input->getArgument('files');
         $list = [];
 
         if (count($zshFiles) > 0) {
-            foreach ($zshFiles as $file) {
-                if (!\file_exists($file)) {
-                    $this->blueStyle->errorMessage('File not found: ' . $file);
-                }
-
-                $handler = \fopen($file, 'rb');
-                $previousStamp = 0;
-
-                while (($line = \fgets($handler)) !== false) {
-                    if ($line === "\n") {
-                        continue;
-                    }
-
-                    \preg_match('# [\d]{10,15}+#', $line, $matches);
-                    $stamp = \trim($matches[0] ?? null);
-
-                    if (!$stamp) {
-                        $list[$previousStamp][] = $line;
-                        continue;
-                    }
-
-                    $previousStamp = $stamp;
-
-                    $data = \explode(':0;', $line);
-                    $list[$stamp][] = $data[1] ?? 'Unresolved command';
-                }
-
-                if (!\feof($handler)) {
-                    $this->blueStyle->errorMessage('Error: unexpected fgets() fail: ' . $file);
-                }
-
-                \fclose($handler);
-            }
+            $list = $this->processFiles($zshFiles);
         }
 
         $history = '';
         \ksort($list);
 
+        $this->blueStyle->infoMessage('Build new history');
+
         foreach ($list as $key => $stamp) {
-            $nextNoStamp = false;
-
-            foreach ($stamp as $line) {
-                if (\preg_match("#\\\\$#", $line)) {
-                    if ($nextNoStamp) {
-                        $history .= " $line";
-                    } else {
-                        $history .= ": $key:0;$line";
-                    }
-                    $nextNoStamp = true;
-                    continue;
-                }
-
-                if ($nextNoStamp) {
-                    $history .= " $line";
-                } else {
-                    $history .= ": $key:0;$line";
-                }
-
-                $nextNoStamp = false;
-            }
+            $history = $this->buildNewHistory($stamp, $history, $key);
         }
 
         if (empty($history)) {
@@ -153,11 +104,115 @@ class ZshHistoryMergTool extends Command
         }
 
         $this->blueStyle->infoMessage('Processed lines: ' . \count($list));
-        
+
         try {
             \file_put_contents($input->getArgument('output'), $history);
         } catch (\Throwable $exception) {
             $this->blueStyle->errorMessage('Error: save file fail: ' . $exception->getMessage());
+            return;
         }
+
+        $this->blueStyle->success('Merged history file saved.');
+    }
+
+    /**
+     * @param $zshFiles
+     * @return array
+     */
+    protected function processFiles($zshFiles): array
+    {
+        $list = [];
+
+        foreach ($zshFiles as $file) {
+            $this->blueStyle->infoMessage('Processing file: ' . $file);
+
+            if (!\file_exists($file)) {
+                $this->blueStyle->errorMessage('File not found: ' . $file);
+            }
+
+            $handler = \fopen($file, 'rb');
+            $list = $this->processLines($list, $handler);
+
+            if (!\feof($handler)) {
+                $this->blueStyle->errorMessage('Error: unexpected fgets() fail: ' . $file);
+            }
+
+            \fclose($handler);
+        }
+
+        return $list;
+    }
+
+    /**
+     * @param array $list
+     * @param $handler
+     * @return array
+     */
+    protected function processLines(array $list, $handler): array
+    {
+        $previousStamp = 0;
+
+        while (($line = \fgets($handler)) !== false) {
+            if ($line === "\n") {
+                continue;
+            }
+
+            \preg_match('# [\d]{10,15}+#', $line, $matches);
+            $stamp = \trim($matches[0] ?? null);
+
+            if (!$stamp) {
+                $list[$previousStamp][] = $line;
+                continue;
+            }
+
+            $previousStamp = $stamp;
+
+            $data = \explode(':0;', $line);
+            $list[$stamp][] = $data[1] ?? 'Unresolved command';
+        }
+
+        return $list;
+    }
+
+    /**
+     * @param array $stamp
+     * @param string $history
+     * @param int $key
+     * @return string
+     */
+    protected function buildNewHistory(array $stamp, string $history, int $key): string
+    {
+        $nextNoStamp = false;
+
+        foreach ($stamp as $line) {
+            if (\preg_match("#\\\\$#", $line)) {
+                $history = $this->generateLine($history, $line, $key, $nextNoStamp);
+                $nextNoStamp = true;
+                continue;
+            }
+
+            $history = $this->generateLine($history, $line, $key, $nextNoStamp);
+            $nextNoStamp = false;
+        }
+
+        return $history;
+    }
+
+    /**
+     * @param string $history
+     * @param string $line
+     * @param int $key
+     * @param bool $nextNoStamp
+     * @return string
+     */
+    protected function generateLine(string $history, string $line, int $key, bool $nextNoStamp): string
+    {
+        if ($nextNoStamp) {
+            $history .= " $line";
+        } else {
+            $history .= ": $key:0;$line";
+        }
+
+        return $history;
     }
 }
