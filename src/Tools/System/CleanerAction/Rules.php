b537e30c53eb8af30adb58b39e8b80b36a636f0f
Michał Adamiak <michal.adamiak@lizardmedia.pl>
2019-04-24 21:11:50 +0200

    Rules checking moved into separate class

--- src/Tools/System/CleanerAction/Rules.php
+++ src/Tools/System/CleanerAction/Rules.php
@@ -6,37 +6,124 @@
 
 namespace ToolsCli\Tools\System\CleanerAction;
 
-class Rules
+class Rules implements RulesInterface
 {
-    public function __construct(array $rules)
+    public const SIZE_SUFFIX = ['k', 'm', 'g', 't', 'p'];
+
+    /**
+     * @var array
+     */
+    protected $rules;
+
+    /**
+     * @var \SplFileInfo
+     */
+    protected $fileInfo;
+
+    /**
+     * @param array $rules
+     * @param \SplFileInfo $fileInfo
+     */
+    public function __construct(array $rules, \SplFileInfo $fileInfo)
     {
-        $regexp = $rules['regexp'];
-        $found = \preg_match($regexp, $fileInfo->getFilename());
+        $this->rules = $rules;
+        $this->fileInfo = $fileInfo;
     }
 
-    public function isValid()
+    /**
+     * @return bool
+     * @throws \Exception
+     */
+    public function isValid(): bool
     {
-        $rules = true;
+        $status = true;
 
-        $rules &= $this->regExpRule();
-        $rules &= $this->timeRule();
-        $rules &= $this->sizeRule();
+        $status &= $this->regExpRule();
+        $status &= $this->timeRule();
+        $status &= $this->sizeRule();
 
-        return !!$rules;
+        return $status;
     }
 
+    /**
+     * @return bool
+     */
     protected function regExpRule(): bool
     {
-        
+        return !!\preg_match($this->rules['regexp'], $this->fileInfo->getFilename());
     }
 
+    /**
+     * @return bool
+     * @throws \Exception
+     */
     protected function timeRule(): bool
     {
-        
+        switch ($this->rules['date-type']) {
+            case 'create':
+                $fileStamp = $this->fileInfo->getCTime();
+                break;
+
+            case 'access':
+                $fileStamp = $this->fileInfo->getATime();
+                break;
+
+            case 'modify':
+                $fileStamp = $this->fileInfo->getMTime();
+                break;
+
+            default:
+                $fileStamp = false;
+                break;
+        }
+
+        if ($fileStamp) {
+            $fileStamp = (new \DateTime())->setTimestamp($fileStamp);
+            $timeDiff = new \DateTime($this->rules['date-time']);
+
+            $stampDiff = $timeDiff->getTimestamp();
+            $stampFile = $fileStamp->getTimestamp();
+
+            if ($stampDiff < $stampFile) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
+    /**
+     * @return bool
+     */
     protected function sizeRule(): bool
     {
-        
+        $valid = \preg_match('/([><]?)([\d]+)([kmgtpKMGTP])/', $this->rules['size'], $matches);
+
+        if ($valid) {
+            $sizeSuffix = \strtolower($matches[3]);
+            $reverse = array_flip(self::SIZE_SUFFIX);
+
+            $valCalculated = $matches[2] * 1024 ** ($reverse[$sizeSuffix] +1);
+
+            switch ($matches[1]) {
+                case '>':
+                    $sizeRuleValid = $this->fileInfo->getSize() > $valCalculated;
+                    break;
+
+                case '<':
+                    $sizeRuleValid = $this->fileInfo->getSize() < $valCalculated;
+                    break;
+
+                default:
+                    $sizeRuleValid = $this->fileInfo->getSize() === $valCalculated;
+                    break;
+            }
+
+            if (!$sizeRuleValid) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
