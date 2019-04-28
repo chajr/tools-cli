a1c28f47ac3221b0bc74ac5a1cf6c88bd3890457
Michał Adamiak <michal.adamiak@lizardmedia.pl>
2019-04-28 13:29:18 +0200

    Added display style for rules interface

--- src/Tools/System/CleanerAction/RulesInterface.php
+++ src/Tools/System/CleanerAction/RulesInterface.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace ToolsCli\Tools\System\CleanerAction;
+use BlueConsole\Style;
 
 interface RulesInterface
 {
     /**
      * @param array $rules
      * @param \SplFileInfo $fileInfo
+     * @param Style $style
      */
-    public function __construct(array $rules, \SplFileInfo $fileInfo);
+    public function __construct(array $rules, \SplFileInfo $fileInfo, Style $style);
 
     /**
      * @return bool
